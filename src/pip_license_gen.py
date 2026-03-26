5cf090a3378b86e376df5950d8b39351440c5537
tikubonn <7812951+Tikubonn@users.noreply.github.com>
2026-03-26 14:23:06 +0900

    update: 不要になった re モジュールのインポート行を削除しました。 update: package_tree_as_list が一意のパッケージリストを返すように変更しました。それに伴い不要になった unique_list 関数を削除しました。 update: 大域変数 LOGGER は非公開変数であるべきなので名称を _LOGGER に変更しました。 update: find_license の名称が複数の値を返すにもかかわらず単数形なのは違和感があったため当該関数の名称を find_licenses に変更しました。 update: dump_license の名称も同様に単数形であることに違和感を感じたため、名称を複数形に変更しました。 update: 旧 dump_license 関数がライセンスの取得に失敗したパッケージを内部で記録しないようにし、都度都度それらの情報をログに出力するように修正しました。 update: 出力形式を指定できる新しいオプション --format 引数を追加しました。当該引数に json を指定することでこれまでのテキスト形式ではなく JSON 形式での出力が可能になります。

--- src/pip_license_gen.py
+++ src/pip_license_gen.py
@@ -1,6 +1,5 @@
 
 import os
-import re 
 import sys
 import json
 import logging
@@ -10,11 +9,11 @@ import opensafer
 import subprocess
 from collections import deque
 
-LOGGER:logging = logging.getLogger(__name__)
+_LOGGER:"logging.Logger" = logging.getLogger(__name__)
 
 def package_tree_as_list (packages:"list[dict[str, typing.Any]]") -> "list[dict[str, typing.Any]]":
 
-  """階層構造になっているパッケージ情報をひとつのリストに列挙します。
+  """階層構造になっているパッケージ情報を一意のリストに列挙します。
 
   Parameters
   ----------
@@ -33,32 +32,11 @@ def package_tree_as_list (packages:"list[dict[str, typing.Any]]") -> "list[dict[
     while deq:
       p = deq.pop()
       deq.extend(reversed(p.get("_dependencies", [])))
-      result.append(p)
+      if p not in result: #列挙済みでなければ末尾に追加する
+        result.append(p)
   return result
 
-def unique_list (source:"list[typing.Any]") -> "list[typing.Any]":
-
-  """一意の値だけを持つリストを作成します。
-
-  Parameters
-  ----------
-  source : list[typing.Any]
-    操作対象となるリストです。
-    本関数は副作用を持たないため、本引数のリストに変化は起こりません。
-
-  Returns
-  -------
-  list[typing.Any]
-    一意の値だけを持つリストです。
-  """
-
-  result = []
-  for index, item in enumerate(source):
-    if item not in source[:index]:
-      result.append(item)
-  return result
-
-def find_license (package:"dict[str, typing.Any]") -> "typing.Generator[tuple[pathlib.Path|None, str], None, None]":
+def find_licenses (package:"dict[str, typing.Any]") -> "typing.Generator[tuple[pathlib.Path|None, str], None, None]":
 
   """パッケージ情報からライセンス情報を取得します。
 
@@ -81,17 +59,20 @@ def find_license (package:"dict[str, typing.Any]") -> "typing.Generator[tuple[pa
       package_files = importlib.metadata.files(package["name"])
       for file in package_files:
         if file.name == license_file:
+
+          _LOGGER.info("Found a license file: file={:s} package={:s}".format(file.as_posix(), package["name"])) #log.
+
           yield file, file.read_text(encoding="utf-8")
       return
     except ModuleNotFoundError:
 
-      LOGGER.error("Could not load module: {:s}({:s})".format(package["name"], dist_name)) #log.
+      _LOGGER.error("Could not load module: {:s}({:s})".format(package["name"], dist_name)) #log.
 
   license = package.get("license", "")
   if license:
     yield None, license
 
-    LOGGER.info("Found a license from metadata, but it may be incomplete as license, so please check your self it: {:s}".format(package["name"]))
+    _LOGGER.warning("Found a license from metadata, but it may be incomplete as license, so please check your self it: {:s}".format(package["name"]))
 
 def dump_header_texts (texts:list[str], *, file:"io.TextIOBase", separator:str):
 
@@ -105,34 +86,50 @@ def dump_header_texts (texts:list[str], *, file:"io.TextIOBase", separator:str):
     print(line, file=file)
   print(separator_text, file=file)
 
-def dump_license (packages:"list[dict[str, typing.Any]]", *, file:"io.TextIOBase", separator:str):
+def dump_licenses_as_text (packages:"list[dict[str, typing.Any]]", *, file:"io.TextIOBase", separator:str):
 
   """複数のパッケージのライセンスを出力します。
   """
 
-  failed_packages = list()
   for package in packages:
-    found_licenses = list(find_license(package))
+    found_licenses = list(find_licenses(package))
     if found_licenses:
-      for license_file, license in find_license(package):
-        headertexts = []
+      for license_file, license in found_licenses:
+        header_texts = []
         if license_file:
-          headertexts.append("License file {!r} of {:s}".format(license_file.name, package["name"]))
+          header_texts.append("License file {:s} of {:s}".format(license_file.name, package["name"]))
         else:
-          headertexts.append("License text of {:s}".format(package["name"]))
+          header_texts.append("License text of {:s}".format(package["name"]))
         project_url = package.get("project_url", [])
         if project_url:
-          headertexts.append("")
-          headertexts.extend(project_url)
-        dump_header_texts(headertexts, file=file, separator=separator)
+          header_texts.append("")
+          header_texts.extend(project_url)
+        dump_header_texts(header_texts, file=file, separator=separator)
         print(file=file)
-        print(license.strip(), file=file)
+        print(license.strip("\n"), file=file)
         print(file=file)
     else:
-      failed_packages.append(package)
-  for package in failed_packages:
 
-    LOGGER.info("Could not find license info into package: {:s}".format(package["name"])) #log.
+      _LOGGER.warning("Could not find license from package: {:s}".format(package["name"])) #log.
+
+def dump_licenses_as_json (packages:"list[dict[str, typing.Any]]", file:"io.TextIOBase"):
+  result = []
+  for package in packages:
+    found_licenses = list(find_licenses(package))
+    if found_licenses:
+      for license_file, license in found_licenses:
+        pkg = package.copy()
+        pkg.setdefault("_found_licenses", [])
+        pkg["_found_licenses"].append({
+          "file": license_file.as_posix(),
+          "text": license.strip("\n")
+        })
+        result.append(pkg)
+    else:
+
+      _LOGGER.warning("Could not find license from package: {:s}".format(package["name"])) #log.
+
+  json.dump(result, file, indent=2)
 
 def get_pip_info_tree (packages:list[str]) -> "list[dict[str, typing.Any]]":
 
@@ -148,20 +145,27 @@ def main ():
   parser.add_argument("-o", "--output-file", type=str, default="", help="Path of output file. (default is stdout).")
   parser.add_argument("--debug", action="store_true", help="If it enabled, this will print out debug log.")
   parser.add_argument("--ignore-packages", nargs="*", default=[], help="List of ignore packages.")
+  parser.add_argument("--format", type=str, choices=["text", "json"], default="text")
   args = parser.parse_args()
   if args.debug:
     logging.basicConfig(level=logging.DEBUG)
   else:
     logging.basicConfig(level=logging.INFO)
   packages = get_pip_info_tree(args.packages)
-  packages = unique_list(package_tree_as_list(packages))
-  packages = [pkg for pkg in packages if pkg["name"] not in args.ignore_packages] #Delete ignored packages.
+  packages = package_tree_as_list(packages)
+  packages = [pkg for pkg in packages if pkg["name"] not in args.ignore_packages] #--ignore-packages 指定があれば除外する
   if args.output_file:
     stream = opensafer.open_safer(args.output_file, "w", encoding="utf-8")
   else:
-    stream = os.fdopen(os.dup(sys.stdout.fileno()), "w", encoding="utf-8")
+    stream = os.fdopen(os.dup(sys.stdout.fileno()), "w", encoding="utf-8") #dup stdout.
   with stream as unwrapped_stream:
-    dump_license(packages, file=unwrapped_stream, separator="=")
+    match args.format:
+      case "text":
+        dump_licenses_as_text(packages, file=unwrapped_stream, separator="=")
+      case "json":
+        dump_licenses_as_json(packages, file=unwrapped_stream)
+      case _:
+        raise ValueError("Given an unknown format: {!r}".format(args.format))
 
 if __name__ == "__main__":
   main()
